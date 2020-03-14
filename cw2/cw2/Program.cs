29e48463e2dd0894544ba5294e948aa80791b80f
Stefan <Stefan@DESKTOP-RD5IAF5>
2020-03-14 18:40:02 +0100

    nic tu nie ma niech pan nawet nie sprawdza

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -9,23 +9,6 @@ namespace cw2
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            var httpClient = new HttpClient();
-            var odpowiedz = await httpClient.GetStringAsync(args[0]);
-            var odpWstringu = odpowiedz.ToString();
-            Console.WriteLine(odpWstringu+ "/n");
-            String pattern = "\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]+\\b";
-            Regex regex = new Regex(pattern);
-            MatchCollection email = regex.Matches(odpowiedz);
-            HashSet<String> set = new HashSet<string>();
-            foreach (Match e in email)
-            {
-                set.Add(e.Value.ToUpper());
-            }
-            foreach(String s in set)
-            {
-                Console.WriteLine(s);
-            }
-
-        }
+        
     }
 }
---
+++ cw2/cw2/data/dod/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2.data.dod
+{
+    class Student
+    {
+    }
+}
